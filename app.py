4efede24db1874d7cf2ae5f5746981b3e82e9a85
Prajjwal maurya <96020071+prajjwal2512@users.noreply.github.com>
2024-07-29 00:19:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
 from keras.models import load_model 
 import matplotlib.dates as mdates
 import streamlit as st 
